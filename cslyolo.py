459fe36e6850c5553dc9cafc848e9af2a1eb3a53
D0352276 <k12501599501@gmail.com>
2021-07-11 14:55:57 +0800

    update

--- cslyolo.py
+++ cslyolo.py
@@ -385,10 +385,10 @@ class CSLLoss(tf.Module):
 
             iou_loss=self._CIoU(true_y,pred_y,true_mask,true_wht)
             coord_loss=self._BboxesLoss(true_y,pred_y,true_mask,true_wht)
-            cnfd_loss_1=self._ConfidenceLoss(pred_y,true_mask,ignore_mask,true_wht)
+            # cnfd_loss_1=self._ConfidenceLoss(pred_y,true_mask,ignore_mask,true_wht)
             cnfd_loss=self._ConfidenceFocalLoss(pred_y,true_mask,ignore_mask,true_wht)
             classes_loss=self._ClassesLoss(true_y,pred_y,true_mask,true_wht)
 
-            loss=iou_loss+coord_loss+cnfd_loss_1+cnfd_loss+classes_loss
+            loss=iou_loss+coord_loss+cnfd_loss+classes_loss
             return loss
         return _CSLLoss
